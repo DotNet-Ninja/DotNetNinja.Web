ce83e3389bd4cfdad9d3ac9a825fb87c9bdad0b1
DotNet Ninja <chaos.monkey@hotmail.com>
2023-02-09 07:41:16 -0600

    Implement DefaultTimeProvider

---
+++ src/DotNetNinja.Web.Core/Services/DefaultTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetNinja.Web.Services;
+
+public class DefaultTimeProvider : ITimeProvider
+{
+    public DateTime Now => DateTime.Now;
+    public DateTimeOffset OffsetNow => DateTimeOffset.Now;
+    public DateTime RequestTime { get; } = DateTime.Now;
+    public DateTimeOffset RequestOffsetTime { get; } = DateTimeOffset.Now;
+}
---
+++ src/DotNetNinja.Web.Core/Services/ITimeProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetNinja.Web.Services;
+
+public interface ITimeProvider
+{
+    DateTime Now { get; }
+    DateTimeOffset OffsetNow { get; }
+    DateTime RequestTime { get; }
+    DateTimeOffset RequestOffsetTime { get; }
+}
